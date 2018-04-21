bee252195163829f6368c5e8f98505002ab99f6d
JPsupiya <Jarupron626@gmail.com>
2018-04-21 16:02:26 +0700

    Dashboard student&Teacher and Coursepage

---
+++ studentDashboard.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bootstrap 4, from LayoutIt!</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	
+
+
+	 <!--For Name title each page-->
+	 <title><?=$page['title'];?></title> 
+	 <!--Bootstarp 4 css and Javascript-->
+	 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	
+	 <!-- Jasny Bootstrap -->
+	 <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	 <link href="css/jasny-bootstrap.min.css" rel="stylesheet" media="screen">
+	 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	 <script src="https://code.jquery.com/jquery.js"></script>
+	 <!-- Include all compiled plugins (below), or include individual files as needed -->
+	 <script src="js/bootstrap.min.js"></script>
+	 <script src="js/jasny-bootstrap.min.js"></script>
+ 
+
+  </head>
+  <body>
+	<!--Background พื้นหลัง -->
+	<style type="text/css">	
+		body{
+			background-image:url(BG1.jpg);
+			background-size:cover;
+			background-attachment:fixed;	
+		}
+		.content{
+			background:black;
+			width : 50%;
+			padding:40px;
+			margin:100px auto;
+		}
+		p{
+			font-size :25px;
+			color :black;
+		}
+		</style>
+				
+		<p>	
+		</p>
+
+		<div class="row">
+				<div class="col-md-1">
+				</div>
+				<div class="col-md-1">
+				</div>
+				<div class="col-md-10">
+		<div class="card border-primary mb-3" style="max-width: 60rem;">
+				<div class="card-body"></div>
+
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-11">
+			<p>
+			</p>
+			<div class="row">
+				<div class="col-md-1">
+				</div>
+				<div class="col-md-8">
+					<h2>
+						Recent update course
+					</h2>
+					<p>
+
+
+
+					</p>
+
+				</div>
+				<div class="col-md-1.7">
+					<div class="dropdown">
+						 
+						<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
+							Sort by
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							 <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-1">
+					<div class="dropdown">
+						 
+						<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
+							Filter
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							 <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-5">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-1">
+				</div>
+				<div class="col-md-11">
+					<div class="jumbotron">
+						<h5>
+							261xxx  LOL in Everyday life
+						</h5>
+						<p>							
+						</p>						
+						<div class="row">
+								<div class="col-md-4">
+								</div>
+								<div class="col-md-5">
+								</div>
+								<div class="col-md-1">
+									 
+									<button type="button" class="btn btn-primary">
+										View assignment	
+									</button>
+								</div>
+								<p>									
+								</p>
+								<div class="col-md-7">
+								</div>
+						</div> 
+					</div>
+					<div class="jumbotron">
+						<h5>
+							262xxx  Printciple of black desert RNG
+						</h5>
+						<p>							
+						</p>						
+						<div class="row">
+								<div class="col-md-4">
+								</div>
+								<div class="col-md-5">
+								</div>
+								<div class="col-md-1">
+										 
+									<button type="button" class="btn btn-primary">
+										View assignment	
+									</button>
+								</div>
+								<p>									
+								</p>
+								<div class="col-md-5">
+								</div>
+						</div> 
+					</div>
+					<div class="jumbotron">
+						<h5>
+							263xxx  Frame rate management
+						</h5>
+						<p>							
+						</p>						
+						<div class="row">
+								<div class="col-md-4">
+								</div>
+								<div class="col-md-5">
+								</div>
+								<div class="col-md-1">
+									<p src="1.png" alt="image description" title="You are not enroll in this course"/>		 
+									<button type="button" class="btn disabled btn-primary">
+										View assignment	
+									</button>
+								</div>
+								<p>									
+								</p>
+								<div class="col-md-5">
+								</div>
+						</div> 
+					</div>
+					<div class="row">
+							<div class="col-md-5">
+							</div>
+							<div class="col-md-5">
+							</div>
+							<div class="col-md-1">
+										 
+								<button type="button" class="btn  btn-primary">
+									See all
+								</button>
+							</div>
+							<p>									
+							</p>
+							<div class="col-md-5">
+							</div>
+							<p>									
+							</p>
+					</div> 
+				</div>
+			</div>
+		</div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
