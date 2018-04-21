8824a5bc0b2d95150936fb688f8e870eb4313677
JPsupiya <Jarupron626@gmail.com>
2018-04-21 20:46:07 +0700

    ลบ Header

--- studentDashboard.php
+++ studentDashboard.php
@@ -1,39 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Bootstrap 4, from LayoutIt!</title>
-
-    <meta name="description" content="Source code generated using layoutit.com">
-    <meta name="author" content="LayoutIt!">
-
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/style.css" rel="stylesheet">
-	
-
-
-	 <!--For Name title each page-->
-	 <title><?=$page['title'];?></title> 
-	 <!--Bootstarp 4 css and Javascript-->
-	 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-	
-	 <!-- Jasny Bootstrap -->
-	 <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-	 <link href="css/jasny-bootstrap.min.css" rel="stylesheet" media="screen">
-	 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-	 <script src="https://code.jquery.com/jquery.js"></script>
-	 <!-- Include all compiled plugins (below), or include individual files as needed -->
-	 <script src="js/bootstrap.min.js"></script>
-	 <script src="js/jasny-bootstrap.min.js"></script>
- 
-
-  </head>
   <body>
 	<!--Background พื้นหลัง -->
 	<style type="text/css">	
@@ -210,4 +175,3 @@
     <script src="js/bootstrap.min.js"></script>
     <script src="js/scripts.js"></script>
   </body>
-</html>
